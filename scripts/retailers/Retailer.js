eb166cf28c24f949dcdd54e0b48331d3ebff481d
Parker Hodge <iparkerhodge@gmail.com>
2020-04-09 18:22:29 -0500

    added gradient

--- scripts/retailers/Retailer.js
+++ scripts/retailers/Retailer.js
@@ -8,18 +8,17 @@ export const RetailHTML = (shop, dist, nurseries, flowers) => {
             <div class="address">${shop.address}</div>
             <div class="location">${shop.city}, ${shop.state}</div>
         </div>
-        <div class="retailer__flowersTitle">Available Flowers</div>
         <div class="retailer__flowers">
             ${flowers.map(flower => FlowerHTML(flower)).join("")}
         </div>
         <div class="retailer__dist">Distributor: ${dist.name}
-            <div class="dist__location">${dist.city}, ${dist.state}</div>
+            <div class="dist__location address">${dist.city}, ${dist.state}</div>
         </div>
         <div class="retailer__nurseries"><div class="nursery__title">Source Nurseries:</div>${nurseries.map(nursery => {
             return `
             <div class="nursery">
                 <div>${nursery.name}</div>
-                <div>${nursery.city}, ${nursery.state}</div>
+                <div class="nursery__loc address">${nursery.city}, ${nursery.state}</div>
             
             </div>
             `
